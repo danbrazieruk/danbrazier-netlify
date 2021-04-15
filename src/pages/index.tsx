beb43eefcea766e4624194c8b78084ba38550ecf
Daniel Brazier <dan@danbrazier.co.uk>
2021-04-15 11:39:20 +0100

    about page content edited

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,10 +13,9 @@ export default function Index() {
       <div className="container">
         <div>
           <h1>
-            Hi, We're Next.js & Netlify<span className="fancy">.</span>
+            Dan Brazier<span className="fancy">.</span>
           </h1>
-          <span className="handle">@nextjs-netlify-blog</span>
-          <h2>A blog template with Next.js and Netlify.</h2>
+          <span className="handle">Strategy, Design & Experience</span>
           <SocialList />
         </div>
       </div>
