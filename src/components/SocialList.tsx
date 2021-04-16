0ccdd98c3855f526a26093290aaccc161f3cc463
Daniel Brazier <dan@danbrazier.co.uk>
2021-04-16 17:20:45 +0100

    debugging social list

--- src/components/SocialList.tsx
+++ src/components/SocialList.tsx
@@ -3,7 +3,7 @@ import Twitter from "../assets/twitter-alt.svg";
 import GitHub from "../assets/github-alt.svg";
 import LinkedIn from "../assets/linkedin-alt.svg";
 import Dribbble from "../assets/dribbble-alt.svg";
-import config from "../lib/config";
+import {config} from "../lib/config";
 
 export function SocialList({}) {
   return (
@@ -29,7 +29,27 @@ export function SocialList({}) {
         <Twitter width={24} height={24} fill={"#222"} />
       </a>
 
-      
+      {/* LinkedIn */}
+      <a
+        title="LinkedIn"
+        href={`https://linkedin.com/in/${config.linkedin_account}`}
+        target="_blank"
+        rel="nofollow noopener"
+      >
+        <LinkedIn width={24} height={24} fill={"#222"} />
+      </a>
+
+      {/* Dribbble */}
+      <a
+        title="Dribbble"
+        href={`https://dribble.com/${config.dribbble_account}`}
+        target="_blank"
+        rel="nofollow noopener"
+      >
+        <Dribbble width={24} height={24} fill={"#222"} />
+      </a>
+
+
 
       <style jsx>{`
         a {
